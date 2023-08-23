e74ef4f3d501105b196d91dd99858445bc34926f
Alex Wilson <alexnjwilson@gmail.com>
2023-08-23 13:47:44 +0100

    completed challenge

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Restaurant.create!({ name: 'The Bear', address: 'Chicago', phone_number: '07958856744', category: 'italian' })
+Restaurant.create!({ name: 'Eat Toyko', address: 'London', phone_number: '07958234745', category: 'japanese' })
+Restaurant.create!({ name: 'Chicken Mama', address: 'Khao lak', phone_number: '079588496785', category: 'chinese' })
+Restaurant.create!({ name: 'Indian Ocean', address: 'Exeter', phone_number: '07958856345', category: 'belgian' })
+Restaurant.create!({ name: 'Speedboat Bar', address: 'London', phone_number: '07958852343', category: 'chinese' })
